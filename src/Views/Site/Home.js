18f19e907279a4b20dca8b924050074abcca06fc
Freja Hillström <frejahillstrom@gmail.com>
2018-04-10 00:07:48 +0200

    Added semantic-ui test

--- src/Views/Site/Home.js
+++ src/Views/Site/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import ReactLogo from '../../Assets/Images/react-logo.svg'
 import { PageTitle } from '../../Lib/Common/Views'
+import { Button } from 'semantic-ui-react'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home-view">
         <header className="home-view-header">
+			<Button>Click Here</Button>
           <img src={ReactLogo} className="home-view-logo" alt="React Logo" />
           <PageTitle />
         </header>
