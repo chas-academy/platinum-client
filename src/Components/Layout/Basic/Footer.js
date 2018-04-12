986c001deb693dd4683e7f6764872799f7baf00f
Freja Hillström <frejahillstrom@Frejas-MacBook-Pro.local>
2018-04-12 17:32:37 +0200

    started on a footer with icons and contact information

--- src/Components/Layout/Basic/Footer.js
+++ src/Components/Layout/Basic/Footer.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Icon, List } from 'semantic-ui-react'
 
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="footer">
-        <div className="container">
-          <Link to="/">{process.env.REACT_APP_SITE_NAME}</Link>
-        </div>
-      </footer>
+        <footer className="footer">
+            <div class="ui center aligned stackable grid">
+                <div class="four column row">
+                    <div class="left floated column">
+                        <b>PLATINUM</b>
+                    </div>
+                    <div class="center floated column">
+                        <Icon circular name='large facebook square' />
+                        <Icon circular name='large twitter square' />
+                        <Icon circular name='large slack' />
+                        <Icon circular name='large instagram' />
+                    </div>
+                    <div class="right floated column">
+                        <List>
+                            <List.Item icon='marker' content='Elektravägen 29' />
+                            <List.Item icon='mail' content={<a href='mailto:mail@mail.com'>mail@mail.com</a>} />
+                            <List.Item icon='phone' content='070-000 00 00'/>
+                        </List>
+                    </div>
+                </div>
+            </div> 
+        </footer>
+
+
+
     )
   }
 }
