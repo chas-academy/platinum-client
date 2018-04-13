d088401b6fd1c8528f05c303652dcc555f0ccb21
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-04-13 12:21:26 +0200

    correct indentention

--- src/Views/Site/Pricing.js
+++ src/Views/Site/Pricing.js
@@ -8,68 +8,68 @@ const Pricing = () => (
 
     <h2 className='headline'>Pricing</h2>
 
-  <Table celled padded unstackable>
-  
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell singleLine></Table.HeaderCell>
-        <Table.HeaderCell textAlign='center'>Free</Table.HeaderCell>
-        <Table.HeaderCell textAlign='center'>One day $3</Table.HeaderCell>
-        <Table.HeaderCell textAlign='center'>Pro $6.99/m</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+	  <Table celled padded unstackable>
+	  
+	    <Table.Header>
+	      <Table.Row>
+	        <Table.HeaderCell singleLine></Table.HeaderCell>
+	        <Table.HeaderCell textAlign='center'>Free</Table.HeaderCell>
+	        <Table.HeaderCell textAlign='center'>One day $3</Table.HeaderCell>
+	        <Table.HeaderCell textAlign='center'>Pro $6.99/m</Table.HeaderCell>
+	      </Table.Row>
+	    </Table.Header>
 
-    <Table.Body>
+	    <Table.Body>
 
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h3' textAlign='center'>Create poll</Header>
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-      </Table.Row>
-    
-    
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h3' textAlign='center'>Share poll</Header>
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-      </Table.Cell>
+	      <Table.Row>
+	        <Table.Cell>
+	          <Header as='h3' textAlign='center'>Create poll</Header>
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	      </Table.Row>
+	    
+	    
+	      <Table.Row>
+	        <Table.Cell>
+	          <Header as='h3' textAlign='center'>Share poll</Header>
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	      </Table.Cell>
 
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Header as='h3' textAlign='center'>Multiple polls</Header>
-        </Table.Cell>
-        <Table.Cell singleLine></Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-        <Table.Cell textAlign='center'>
-          <Icon color='green' name='checkmark' size='large' />
-        </Table.Cell>
-      </Table.Row>
-      
-    </Table.Body>
+	      </Table.Row>
+	      <Table.Row>
+	        <Table.Cell>
+	          <Header as='h3' textAlign='center'>Multiple polls</Header>
+	        </Table.Cell>
+	        <Table.Cell singleLine></Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	        <Table.Cell textAlign='center'>
+	          <Icon color='green' name='checkmark' size='large' />
+	        </Table.Cell>
+	      </Table.Row>
+	      
+	    </Table.Body>
 
-  </Table>
+	  </Table>
 
-  <GetStarted />
+	<GetStarted />
   
   </div>
 )
