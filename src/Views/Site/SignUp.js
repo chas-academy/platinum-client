d434b2a0f52e36ef4f71ab2af03dc8ed7f4a98c4
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-04-15 22:24:21 +0200

    Include sign up form in the view

--- src/Views/Site/SignUp.js
+++ src/Views/Site/SignUp.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { PageTitle } from '../../Lib/Common/Views'
+import SignUpForm from "../../Components/Forms/SignUp";
 
-export default class SignIn extends Component {
+
+export default class SignUp extends Component {
   render() {
     return (
       <div className="sign-in-view">
         <PageTitle title="Sign Up" />
-        
+        <SignUpForm />
       </div>
     )
   }
