1bef0310023e97cc77241eaaff4d0caa5beb2464
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-15 16:47:58 +0200

    improve responsivenes for createQuestion view

--- src/Components/Forms/Question/CreateQuestion.js
+++ src/Components/Forms/Question/CreateQuestion.js
@@ -10,19 +10,19 @@ export default class CreateQuestion extends Component {
       options: [
         <Form.Group
           key={uuidv1()}
-          className="center-content"
+          className="center-content padding-b-1"
           unstackable
           widths={2}
         >
-          <Option name="option1" width={16} />
+          <Option name="option1" width={11} />
         </Form.Group>,
         <Form.Group
           key={uuidv1()}
-          className="center-content"
+          className="center-content padding-b-1"
           unstackable
           widths={2}
         >
-          <Option name="option2" width={16} />
+          <Option name="option2" width={11} />
         </Form.Group>,
       ],
     };
@@ -35,16 +35,17 @@ export default class CreateQuestion extends Component {
       options: [
         ...this.state.options,
         <Form.Group
-          className="center-content"
+          className="center-content padding-b-1"
           key={uuidv1()}
           unstackable
           widths={2}
         >
-          <Option name={`option${this.state.options.length + 1}`} width={15} />
+          <Option name={`option${this.state.options.length + 1}`} width={10} />
           <Form.Button
             width={1}
             type="button"
             negative
+            compact
             content="X"
             value={this.state.options.length}
             onClick={this.removeOption}
@@ -68,7 +69,11 @@ export default class CreateQuestion extends Component {
         <div className="min-height">
           <Form id="creat-question-form">
             <div className="padding-tb-2">
-              <Form.Group className="center-content" widths={2}>
+              <Form.Group
+                className="center-content padding-b-1"
+                widths={2}
+                unstackable
+              >
                 <Form.Input
                   className="center"
                   onChange={this.handleChange}
@@ -77,7 +82,7 @@ export default class CreateQuestion extends Component {
                   placeholder="Question"
                   required
                   type="text"
-                  width={16}
+                  width={11}
                 />
               </Form.Group>
               {this.state.options}
