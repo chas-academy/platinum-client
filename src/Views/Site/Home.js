f26f7983c76d5b905e9f2c2ecc9db55e99b6640a
Freja Hillström <frejahillstrom@Frejas-MacBook-Pro.local>
2018-04-15 23:39:15 +0200

    made the buttons responsive

--- src/Views/Site/Home.js
+++ src/Views/Site/Home.js
@@ -7,16 +7,16 @@ export default class Home extends Component {
             <div className="home-view">
                 <Header as='h1'>PLATINUM</Header>
 
-                <div class="ui  two column centered grid">
-                    <div class="four column centered row">    
-                        <div class="column middle aligned">
+                <div className="ui two column centered grid stackable columns">
+                    <div className="four column centered row">    
+                        <div className="column middle aligned">
                             <Button fluid inverted color='orange'>LOG IN</Button>
                         </div>
-                        <div class="column middle aligned">
+                        <div className="column middle aligned">
                             <Button fluid inverted color='red'>REGISTER</Button>
                         </div>
                     </div>
-                    <div class="two column middle aligned">
+                    <div className="two column middle aligned">
                         <Button fluid inverted color="pink">WHAT WE DO</Button>
                     </div>
                 </div>
