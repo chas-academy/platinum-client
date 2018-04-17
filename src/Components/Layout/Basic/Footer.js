a85f081e06236dd6bcea28004fe85be69b96313e
Freja Hillström <frejahillstrom@Frejas-MacBook-Pro.local>
2018-04-17 15:34:05 +0200

    fixed issues with landscape mode, and centered layout with smaller screens

--- src/Components/Layout/Basic/Footer.js
+++ src/Components/Layout/Basic/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Icon, List, Grid, Container } from 'semantic-ui-react'
+import { Icon, List, Grid } from 'semantic-ui-react'
 
 /* eslint-disable react/prefer-stateless-function, jsx-a11y/anchor-is-valid */
 
@@ -23,7 +23,7 @@ export default class Footer extends Component {
                     <Grid columns={2} centered>
                         <Grid.Row centered>                            
                             <Grid.Column>
-                                <List>
+                                <List className="center-content">
                                     <List.Item icon="marker" content="Elektravägen 29" />
                                     <List.Item icon='mail' content={<a href='mailto:mail@mail.com'>mail@mail.com</a>} />
                                     <List.Item icon='phone' content='070-000 00 00'/>
@@ -34,9 +34,6 @@ export default class Footer extends Component {
                 </Grid.Column>
             </Grid>
         </footer>
-
-
-
     )
   }
 }
