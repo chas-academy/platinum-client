8a22dafaae65e8f23cad51d6df69bf4c903464b8
bob <sophie.granloef@gmail.com>
2018-04-17 14:47:09 +0200

    add import MyQuestion from '../../Components/Forms/ViewQuestion

---
+++ src/Views/Site/ListView.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react'
+import MyQuestion from '../../Components/Form/ViewQuestion';
+
+export default class ListView extends Component {
+    render() {
+      return (
+        <MyQuestion />
+      )
+    }
+  }
+  
