1c22ae12f07ce266fc63a3e21d1ca32b9280ac60
bob <sophie.granloef@gmail.com>
2018-04-17 14:16:20 +0200

    added file MyQuestion.scss

--- src/Components/Forms/ViewQuestion/MyQuestion.js
+++ src/Components/Forms/ViewQuestion/MyQuestion.js
@@ -1,7 +1,25 @@
+
+
 import React, { Component } from 'react'
-import { Accordion } from "semantic-ui-react";
+import { Accordion, Icon } from 'semantic-ui-react'
 
+export default class MyQuestion extends Component {
+  state = { activeIndex: 0 }
 
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    const { activeIndex } = this.state
+
+    return (
+      <form class="ui-form">
+      
 <div class="ui vertical menu"></div>
     <div class="header item">Question</div>
   <div class="ui styled fluid accordion"></div>
@@ -14,17 +32,18 @@ import { Accordion } from "semantic-ui-react";
     
     <div class="title">
       <i class="dropdown icon"></i>
-     Poll 1
+     
     </div>
     <div class="content">
       
     </div>
     <div class="title active">
       <i class="dropdown icon"></i>
-      How do you acquire a dog?
+      
     </div>
     <div class="content active">
     </div>
-   );
+    </form>
+    );
   }
 }
