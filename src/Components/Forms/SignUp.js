269cf6ea172f5c3624c9578f81b4621c224bafb2
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-04-18 16:11:14 +0200

    Add a new sign up form using jsonschema-form

--- src/Components/Forms/SignUp.js
+++ src/Components/Forms/SignUp.js
@@ -1,23 +1,201 @@
 import React, { Component } from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import JWT from 'jsonwebtoken'
+import Form from 'react-jsonschema-form'
+import Alert from '../Alert'
+import Axios from '../../Lib/Common/Axios'
+import * as FormHelper from '../../Lib/Helpers/Form'
+import * as Session from '../../Lib/Helpers/Session'
 
+export default class SignUp extends Component {
 
-export default class SignUpForm extends Component {
-    
-    render() {
-        return (
-            <Form>
-            <Form.Field>
-              <label>Email</label>
-              <input placeholder='Email' />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input placeholder='Password' />
-            </Form.Field>
+  constructor(props) {
+    super(props)
 
-            <Button type='submit'>SIGN UP</Button>
-          </Form>
-        );
+    this.onSubmit = this.onSubmit.bind(this)
+    this.state = {
+      key: Date.now(),
+      formData: initFormData,
+      alertMessage: {},
+      showAlertMessage: false,
+      isSigningIn: false,
+
     }
+  }
+
+  onSubmit({ formData }) {
+    this.setState({
+      key: Date.now(),
+      formData: formData,
+      alertMessage: {
+        type: 'info',
+        message: 'Signing up...'
+      },
+      showAlertMessage: true
+    })
+
+    const token = JWT.sign(formData, process.env.REACT_APP_API_JWT_SECRET)
+
+    Axios
+      .post(process.env.REACT_APP_API_SIGN_IN_URL, { token })
+      .then(response => {
+        const { token, redirect } = response.data
+
+        Session.store({ token })
+
+        this.setState({
+          alertMessage: {
+            type: 'success',
+            message: 'Redirecting...'
+          },
+          redirect: redirect ? redirect : this.state.redirect
+        })
+
+        if (!this.state.locationState && redirect && redirect.indexOf('http') === 0)
+          return window.location.href = redirect
+
+        const _this = this
+
+        setTimeout(function(){
+          _this.setState({
+            formData: initFormData,
+            isSigningIn: false
+          })
+          _this.props.auth(true);
+        }, 500)
+      })
+      .catch(error => {
+        let message  = 'Unable to process your request. Please check your internet connection. If problem persists, contact support.'
+
+        if (error.response && error.response.data.message)
+          message = error.response.data.message
+
+        console.log('Error: ', error)
+
+        this.setState({
+          alertMessage: {
+            type: 'danger',
+            message: message
+          },
+          showAlertMessage: true,
+          isSigningIn: false
+        })
+      })
+  }
+
+  render() {
+    // if (Session.token() && this.props.IsSignedIn) {
+    //   const locationState = this.state.locationState
+    //   let referrer = this.state.redirect;
+
+    //   if (locationState && locationState.from) {
+    //     const { pathname, search } = locationState.from
+    //     referrer = [pathname, search].join('')
+    //   }
+
+    //   return <Redirect to={referrer} />
+    // }
+
+    return (
+      <div className="form-wrapper">
+        {this.state.showAlertMessage &&
+          <Alert type={this.state.alertMessage.type} hideDismissButton>
+            <p>{this.state.alertMessage.message}</p>
+          </Alert>
+        }
+        <Form
+          className="form"
+          autocomplete="off"
+          key={this.state.key}
+          formData={this.state.formData}
+          schema={Schema}
+          uiSchema={UISchema}
+          validate={validate}
+          ErrorList={FormHelper.errorList}
+          onSubmit={this.onSubmit}
+        >
+          <button
+            type="submit"
+            className="btn btn-primary form-control"
+            disabled={this.state.isSigningIn}
+          >
+            Sign Up
+          </button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const initFormData = {
+  email: '',
+  password: ''
+}
+
+const Schema = {
+  'type': 'object',
+  'properties': {
+    'first name': {
+      'type': 'string',
+      'title': 'First Name'
+    },
+    'surname': {
+      'type': 'string',
+      'title': 'Surname'
+    },
+    'email': {
+      'type': 'string',
+      'title': 'Email'
+    },
+    'password': {
+      'type': 'string',
+      'title': 'Password'
+    },
+    'confirm password': {
+      'type': 'string',
+      'title': 'Confirm Password'
+    }
+  }
+}
+
+const UISchema = {
+  'ui:rootFieldId': 'log_in',
+  'first name': {
+    'ui:autofocus': true,
+    'ui:placeholder': 'Enter your first name'
+  },
+  'surname': {
+    'ui:autofocus': true,
+    'ui:placeholder': 'Enter your surname'
+  },
+  'email': {
+    'ui:widget': 'email',
+    'ui:placeholder': 'Enter your email'
+  },
+  'password': {
+    'ui:widget': 'password',
+    'ui:placeholder': 'Enter your password'
+  },
+  'confirm password': {
+    'ui:widget': 'password',
+    'ui:placeholder': 'Enter your password again'
+  }
+}
+
+function validate(formData, errors) {
+  let input
+
+  if (formData.email === undefined || formData.email === '') {
+    errors.email.addError('Email is required.')
+    input = 'email'
+  }
+
+  if (formData.password === undefined || formData.password === '') {
+    errors.password.addError('Password is required.')
+    input = input || 'password'
+  }
+
+  FormHelper.setFocus(UISchema, input)
+
+  return errors
+
 }
