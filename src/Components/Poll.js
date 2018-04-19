0addacb87c924b6c184661f820d0b85594f0e481
bob <sophie.granloef@gmail.com>
2018-04-19 13:56:39 +0200

    add style to the MyQuestion.js

--- src/Components/Poll.js
+++ src/Components/Poll.js
@@ -17,14 +17,15 @@ export default class Poll extends Component {
 
   render() {
     return(
-        <div >
-            <button class="ui blue basic button" class ="btn btn-default">Edit</button>
-            <button class="ui orange basic button" class="btn btn-default" onClick={this.togglePoll}>Activate</button>
+        <div class="button-group">
+            <button class="ui blue basic button"  >Edit</button>
+            <button class="ui orange basic button"  onClick={this.togglePoll}>Activate</button>
+            
             { this.state.active &&
             <div>
                 <h3>Share Poll</h3>
-                <button class="ui basic button yellow" class="btn btn-default" >Link</button>
-                <button class="ui basic button yellow" class="btn btn-default" >QR-Code</button>
+                <button class="ui basic button yellow" >Link</button>
+                <button class="ui basic button yellow"  >QR-Code</button>
             </div>
             }
             
