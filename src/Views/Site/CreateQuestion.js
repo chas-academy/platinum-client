5db8454343bef71c73b8ba2c9c612b9571f6503f
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-04-19 15:31:18 +0200

    fix husky issues

--- src/Views/Site/CreateQuestion.js
+++ src/Views/Site/CreateQuestion.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import Question from "../../Components/Forms/Question/CreateQuestion";
+import React from 'react';
+import Question from '../../Components/Forms/Question/CreateQuestion';
 
-export default class CreateQuestion extends Component {
-  render() {
-    return <Question />;
-  }
-}
+const CreateQuestion = () => (
+  <Question />
+);
+
+export default CreateQuestion;
