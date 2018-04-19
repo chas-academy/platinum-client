3f2a965f234803beea706de5ce12d42190709499
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-04-19 13:24:35 +0200

    add margin to about div

--- src/Views/Site/About.js
+++ src/Views/Site/About.js
@@ -3,25 +3,24 @@ import GetStarted from '../../Components/Buttons/GetStarted';
 import { Message } from 'semantic-ui-react';
 
 const About = () => (
-  <div className="ui center aligned basic segment">
+  <div className="ui center aligned basic segment grid">
 
     <h2 className="headline">About us</h2>
 
-    <div className="ui center aligned basic segment parentbox">
-
+    <div className="parentbox">
       <Message compact className="aboutbox">
-        Which mood are you in today&#63;
-        Fun or serious? Don’t worry
-        you don’t have to choose -
-        because you can use this app
-        just for fun or also in some
-        serious business. Great huh?!
+          Which mood are you in today&#63;
+          Fun or serious? Don’t worry
+          you don’t have to choose -
+          because you can use this app
+          just for fun or also in some
+          serious business. Great huh?!
 
-        Just click the button and you
-        are ready to create your first
-        poll for free which you can
-        share with whoever! It’s super
-        easy, we promise. And fun too!
+          Just click the button and you
+          are ready to create your first
+          poll for free which you can
+          share with whoever! It’s super
+          easy, we promise. And fun too!
       </Message>
     </div>
 
