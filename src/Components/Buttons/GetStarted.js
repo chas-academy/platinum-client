e06cd6c71c352548a0366bd71cb0f723de87dd34
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-24 00:33:44 +0200

    modify footer position

--- src/Components/Buttons/GetStarted.js
+++ src/Components/Buttons/GetStarted.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import { Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react';
 
-//onClick={this.redirect('/start') basically linking to the start spage.
-//maybe there's a better way than using this one-line crap
+// onClick={this.redirect('/start') basically linking to the start spage.
+// maybe there's a better way than using this one-line crap
 
 const GetStarted = () => (
-  <div className='ui center aligned basic segment'>
+  <div className="ui center aligned basic segment">
 
-      <Button padded centered size='huge' className='centered'>
-          Let's get started!
-      </Button>
-  
+    <Button padded centered size="huge" className="centered">{'Let\'s get started!'}</Button>
+
   </div>
-)
+);
 
-export default GetStarted
+export default GetStarted;
+
