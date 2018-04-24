e7a72de381e691bc5d79eb9e3f37d830aa07542f
Jonas Töremar <jonas.toremar@chasacademy.se>
2018-04-24 12:57:05 +0200

    change folder structure

--- src/Views/Site/About.js
+++ src/Views/Site/About.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import GetStarted from '../../Components/Buttons/GetStarted';
-import { Message } from 'semantic-ui-react';
+import AboutInfo from '../../Components/Info/About';
+import { PageTitle } from '../../Lib/Common/Views';
 
 const About = () => (
   <div className="ui center aligned basic segment grid">
 
-    <h2 className="headline">About us</h2>
+    <PageTitle title="About us" />
 
-    <div className="parentbox">
-      <Message compact className="aboutbox">
-          Which mood are you in today&#63;
-          Fun or serious? Don’t worry
-          you don’t have to choose -
-          because you can use this app
-          just for fun or also in some
-          serious business. Great huh?!
-
-          Just click the button and you
-          are ready to create your first
-          poll for free which you can
-          share with whoever! It’s super
-          easy, we promise. And fun too!
-      </Message>
-    </div>
+    <AboutInfo />
 
     <GetStarted />
 
---
+++ src/Views/Site/Pricing.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import GetStarted from '../../Components/Buttons/GetStarted';
+import { Header, Table, Icon } from 'semantic-ui-react';
+
+const Pricing = () => (
+  <div className="ui center aligned basic segment parent-container">
+
+    <h2 className="headline">Pricing</h2>
+
+    <div className="table-container">
+
+      <Table celled padded unstackable className="table">
+
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell singleLine />
+            <Table.HeaderCell textAlign="center">Free</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">One day $3</Table.HeaderCell>
+            <Table.HeaderCell textAlign="center">Pro $6.99/m</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+
+          <Table.Row>
+            <Table.Cell>
+              <Header as="h3" textAlign="center">Create poll</Header>
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+          </Table.Row>
+
+
+          <Table.Row>
+            <Table.Cell>
+              <Header as="h3" textAlign="center">Share poll</Header>
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <Header as="h3" textAlign="center">Multiple polls</Header>
+            </Table.Cell>
+            <Table.Cell singleLine />
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+            <Table.Cell textAlign="center">
+              <Icon color="green" name="checkmark" size="large" />
+            </Table.Cell>
+          </Table.Row>
+
+        </Table.Body>
+
+      </Table>
+
+      <GetStarted />
+
+    </div>
+
+  </div>
+);
+
+export default Pricing;
