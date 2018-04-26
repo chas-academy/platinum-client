954da639a9508a2d557197d0d85e307dd1fa0380
bob <sophie.granloef@gmail.com>
2018-04-26 14:17:41 +0200

    add some rows in Resultview

--- src/Views/Site/ResultView.js
+++ src/Views/Site/ResultView.js
@@ -3,39 +3,27 @@ import React, { Component } from 'react'
 import {Container, Header} from 'semantic-ui-react'
 
 class ResultView extends React.Component{
-
-render(){
-
-    const result = this.props.result;
-    return(
-        <tr>
-            <th colspan="">
-            {result}
-            </th>
-            </tr>
-
-    );
-}
-}
-
-class ResultView extends React.Component{
-    rendeer(){
-        const result = this.props.product;
-        const poll = poll.result ?
-        result.poll :
-        <span style={{color: 'black'}}>
-        {{poll.result}}
-        </span>;
-
-        return(
-
-            <tr>
-                <td>{result}</td>
-                <td>{result.poll}</td>
-</tr>
-        );
-    }
-}
-
-
-
+ 
+    let DashboardApp = React.createClass({
+        render: function() {
+          return (
+            <div>
+              <Header as="h2"className="main-title">Acme Inc. Revenue Analysis for 2015</Header>
+              <div id="interactive-dashbaord"></div>
+              <div className="chart-row">
+                <div id="country-revenue">
+                  // country revenue chart here
+                </div>
+              </div>
+              <div className="chart-row">
+                <div id="monthly-revenue" className="inline-chart">
+                  // chart 2 here - spline
+                </div>
+                <div id="product-revenue" className="inline-chart">
+                  // chart 3 here - donut
+                </div>
+              </div>
+            </div>
+          );
+        }
+      });
