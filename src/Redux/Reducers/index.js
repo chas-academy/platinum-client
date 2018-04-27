568fc99a5037bab419dad7958b345820fab3dd9b
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-27 17:05:18 +0200

    add Poll-reducer to index.js to be exported with existing reducers

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,9 +1,11 @@
 import Admin from './Admin';
 import IsSignedIn from './IsSignedIn';
 import Students from './Students';
+import Polls from './Polls';
 
 export default {
   AdminUsers: Admin.Users,
   IsSignedIn,
   Students,
+  Polls,
 };
