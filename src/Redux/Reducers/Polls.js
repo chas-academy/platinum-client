f7b7c731f16e1f590752982a71b487985a62f6ef
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-04-29 14:59:57 +0200

    update reducer
    	* remove setting of activ poll when creating a poll
    	* remove adding a poll to closedpoll shen closing a poll

--- src/Redux/Reducers/Polls.js
+++ src/Redux/Reducers/Polls.js
@@ -1,10 +1,8 @@
 import Immutable from 'immutable';
 import actionTypes from '../Actions/Polls/Types';
-import Poll from '../Models/Poll';
 
 const DEFAULT_STATE = {
   activePolls: Immutable.Record(),
-  closedPolls: Immutable.OrderedMap(),
   isActivatingPoll: false,
   isClosingPoll: false,
 };
@@ -14,17 +12,13 @@ export default function (state = DEFAULT_STATE, action) {
     case actionTypes.ACTIVATE_POLL_START:
       return { ...state, isActivatingPoll: true };
     case actionTypes.ACTIVATE_POLL_SUCCESS:
-      return { ...state, activePolls: new Poll(action.poll), isActivatingPoll: false };
+      return { ...state, isActivatingPoll: false };
     case actionTypes.ACTIVATE_POLL_FAILURE:
       return { ...state, isActivatingPoll: false };
     case actionTypes.CLOSE_POLL_START:
       return { ...state, isClosingPoll: true };
     case actionTypes.CLOSE_POLL_SUCCESS:
-      return {
-        ...state,
-        closedPolls: [...state.closedPolls, new Poll(action.poll)],
-        isClosingPoll: false,
-      };
+      return { ...state, isClosingPoll: false };
     case actionTypes.CLOSE_POLL_FAILURE:
       return { ...state, isClosingPoll: false };
     default:
