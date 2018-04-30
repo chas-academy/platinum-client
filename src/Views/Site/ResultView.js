676bd0a05dfb6a5160aadeebe9511ebfd480bcbd
bob <sophie.granloef@gmail.com>
2018-04-30 15:50:59 +0200

    add style for the chart.js

--- src/Views/Site/ResultView.js
+++ src/Views/Site/ResultView.js
@@ -4,26 +4,43 @@ import {Container, Header} from 'semantic-ui-react'
 
 class ResultView extends React.Component{
  
-    let DashboardApp = React.createClass({
-        render: function() {
-          return (
-            <div>
-              <Header as="h2"className="main-title">Acme Inc. Revenue Analysis for 2015</Header>
-              <div id="interactive-dashbaord"></div>
-              <div className="chart-row">
-                <div id="country-revenue">
-                  // country revenue chart here
-                </div>
-              </div>
-              <div className="chart-row">
-                <div id="monthly-revenue" className="inline-chart">
-                  // chart 2 here - spline
-                </div>
-                <div id="product-revenue" className="inline-chart">
-                  // chart 3 here - donut
-                </div>
-              </div>
-            </div>
-          );
-        }
-      });
+
+  constructor(props){
+    super(props);
+    this.state ={
+      chartData:{
+        labels: ['Example 1', 'Example 2', 'Example 3'],
+          data:[
+
+            617689,
+            654890,
+            925654
+          ],
+          backgroundColor:  [// Dummy data for the chart
+          'rgba(255, 99, 132, 0.2)', //dummy color for the background-color in the chart
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+          ]
+      }
+    }
+  }
+  render(){
+    return (
+      <div className="chart">
+      <Bar
+      data={this.state.chartData}
+      width={100}
+      height={50}
+      options={{
+      }}
+      />
+      </div>
+    )
+  }
+  }
