c421f7bfd8de52016f4b222bb2d8a6b4d7169c34
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-05-02 16:19:45 +0200

    Import questionnaires to the central reducer file

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -2,10 +2,12 @@ import Admin from './Admin';
 import IsSignedIn from './IsSignedIn';
 import Students from './Students';
 import Polls from './Polls';
+import Questionnaires from './Questionnaires';
 
 export default {
   AdminUsers: Admin.Users,
   IsSignedIn,
   Students,
   Polls,
+  Questionnaires,
 };
