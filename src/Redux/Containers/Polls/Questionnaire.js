749ac5edd7dac291237a5b7038ef04d0983570cb
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-05-03 08:25:36 +0200

    Add fetched questionnaires to polls container
    	- To be able to get the data to the front end to check if a poll is active or not.

--- src/Redux/Containers/Polls/Questionnaire.js
+++ src/Redux/Containers/Polls/Questionnaire.js
@@ -1,11 +1,13 @@
 import { connect } from 'react-redux';
 import { activatePoll, closePoll } from '../../Actions/Polls/';
+import { fetchQuestionnaires } from '../../Actions/Questionnaires/';
 import Questionnaire from '../../../Components/Questionnaires/Questionnaire';
 
 function mapDispatchToProps(dispatch) {
   return {
     activatePoll: questionnaireId => dispatch(activatePoll(questionnaireId)),
     closePoll: id => dispatch(closePoll(id)),
+    fetchQuestionnaires: () => dispatch(fetchQuestionnaires()),
   };
 }
 
