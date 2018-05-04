fb9d1b34a2db29c0e82274b6d7746bc2c62ad86c
jonastore <jonas.toremar@chasacademy.se>
2018-05-04 12:28:18 +0200

    fix typos on buttons

--- src/Views/Site/Home.js
+++ src/Views/Site/Home.js
@@ -21,7 +21,7 @@ export default class Home extends Component {
                 </div>
               </div>
               <div className="two column middle aligned">
-                <Button href="/howdoesitwork" fluid inverted color="orange">ABOUT US</Button>
+                <Button href="/howdoesitwork" fluid inverted color="orange">HOW DOES IT WORK?</Button>
               </div>
             </div>
           </div>
@@ -35,7 +35,7 @@ export default class Home extends Component {
               <div className="ui two column centered grid stackable columns">
                 <div className="four column centered row">
                   <div className="column middle aligned">
-                    <Button href="/create-questionnaire" fluid inverted color="orange">WHAT WE DO!</Button>
+                    <Button href="/create-questionnaire" fluid inverted color="orange">LET&apos;S GET STARTED!</Button>
                   </div>
                 </div>
               </div>
