b6543022af9ad6603513b65db47bb20f64ed1ecb
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-05-07 20:19:01 +0200

    Show options and votes in a bar chart

--- src/Components/Results/BarChart.js
+++ src/Components/Results/BarChart.js
@@ -4,27 +4,23 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 /* eslint-disable react/prop-types, react/prefer-stateless-function */
 export default class BarChartResult extends Component {
   render() {
-    const data = [
-      {
-        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-      },
-      {
-        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-      },
-      {
-        name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-      },
-    ];
+    const data = [];
+    this.props.options.forEach((element) => {
+      const option = {
+        name: element.name,
+        votes: element.votes,
+      };
+      data.push(option);
+    });
+
     return (
       <BarChart width={600} height={300} data={data}>
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar type="monotone" dataKey="pv" barSize={40} fill="#000" />
+        <Bar type="monotone" dataKey="votes" barSize={40} fill="#000" />
       </BarChart>
     );
   }
 }
-
-/* eslint-enable react/prop-types */
