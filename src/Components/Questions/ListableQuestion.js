35085c216bc0a0b49bc2ddbe21e5a4b69f840160
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-10 20:29:49 +0200

    add func to enable editing
    	* add button with on click that triggers createQuestion component to be renderd
    	* send question data to createQuestion component

--- src/Components/Questions/ListableQuestion.js
+++ src/Components/Questions/ListableQuestion.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import CreateQuestion from '../../Redux/Containers/Questionnaires/CreateQuestion';
 /* eslint-disable */
 export default class  ListableQuestion extends Component {
 
   constructor(props){
     super(props);
-    
+    this.editQuestion = this.editQuestion.bind(this);
+    this.state ={
+      isBeingEdited: false,
+    }
+  }
+
+  editQuestion(){
+    this.setState({
+      isBeingEdited: true,
+    })
   }
   render() {
     return (
+    
       <div className="margin-tb-1">
-        <h3>{this.props.name}</h3>
+        { !this.state.isBeingEdited &&
+        <div>
+        <h3>{this.props.question.name}</h3>
+        <Button basic content="edit" onClick={this.editQuestion} />
+        </div>
+        }
+        { this.state.isBeingEdited &&
+        <div>
+          <CreateQuestion question={this.props.question}/>
+        </div>
+        }
       </div>
+      
     )
   }
 }
