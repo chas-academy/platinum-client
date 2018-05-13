9f4ff1372719d37154cf3f37b3a45edf777b354c
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-13 17:52:28 +0200

    Enable multiselect answers
    	* add func to check if question is of type multiselect
    	* add addOption function to be used instead of selectOption if type is multiselect
    	* add paragrap tag to let users se what type of question they are answering

--- src/Components/Poll/Answer/Vote/Vote.js
+++ src/Components/Poll/Answer/Vote/Vote.js
@@ -8,17 +8,24 @@ export default class Vote extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedOption: null,
+      selectedOptions: [],
       errorMessage: '',
+      multiselect: false,
     };
     this.toggleOption = this.toggleOption.bind(this);
     this.castVote = this.castVote.bind(this);
+    this.addOption = this.addOption.bind(this);
   }
   componentWillReceiveProps(nextProps) {
     if (nextProps.question.id !== this.props.question.id) {
       this.setState({
-        selectedOption: null,
+        selectedOptions: null,
         errorMessage: '',
+      });
+    }
+    if (nextProps.question.type === 'multiselect') {
+      this.setState({
+        multiselect: true,
       });
     }
   }
@@ -30,14 +37,41 @@ export default class Vote extends Component {
   }
   toggleOption(id) {
     this.setState({
-      selectedOption: id,
+      selectedOptions: [id],
     });
   }
+  addOption(id) {
+    let toBeAdded = true;
+    const selectedOptions = [...this.state.selectedOptions];
+
+    selectedOptions.map((optionId, index) => {
+      if (optionId === id) {
+        toBeAdded = false;
+        selectedOptions.splice(index, 1);
+      }
+      return optionId;
+    });
+
+    if (toBeAdded) {
+      this.setState({
+        selectedOptions: [
+          ...selectedOptions,
+          id,
+        ],
+      });
+    } else {
+      this.setState({
+        selectedOptions: [
+          ...selectedOptions,
+        ],
+      });
+    }
+  }
   castVote() {
-    if (this.state.selectedOption !== null) {
+    if (this.state.selectedOptions.length !== 0) {
       const vote = {
         questionId: this.props.question.id,
-        optionId: this.state.selectedOption,
+        optionIds: this.state.selectedOptions,
         answerId: this.props.answer.id,
         pollId: this.props.answer.pollId,
       };
@@ -56,7 +90,7 @@ export default class Vote extends Component {
     const options = [];
     this.props.question.options.forEach((option) => {
       let color = 'grey';
-      if (option.id === this.state.selectedOption) {
+      if (this.state.selectedOptions.includes(option.id)) {
         color = 'green';
       }
       const newOption = (
@@ -64,15 +98,15 @@ export default class Vote extends Component {
           key={uuidv4()}
           option={option}
           color={color}
-          select={this.toggleOption}
+          select={this.state.multiselect ? this.addOption : this.toggleOption}
         />);
 
       options.push(newOption);
     });
-
     return (
       <div className="center-content-column">
         <h1 className="text-allign-center margin-b-3">{this.props.question.name}</h1>
+        <p className="text-allign-center margin-b-3">{this.props.question.type}</p>
         {options}
         <Message content={this.state.errorMessage} hidden={this.state.errorMessage === ''} />
         <div className="min-width-15 margin-t-2">
