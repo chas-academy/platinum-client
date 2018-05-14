63bded0ffd6fd4979af15ee8d64d29ce9a1bd942
jonastore <jonas.toremar@chasacademy.se>
2018-05-14 14:13:06 +0200

    add link that redirect to the sign-in page

--- src/Components/Buttons/GetStarted.js
+++ src/Components/Buttons/GetStarted.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 // onClick={this.redirect('/start') basically linking to the start spage.
 // maybe there's a better way than using this one-line crap
@@ -7,7 +8,7 @@ import { Button } from 'semantic-ui-react';
 const GetStarted = () => (
   <div className="ui center aligned basic segment">
 
-    <Button size="huge" className="centered">{'Let\'s get started!'}</Button>
+    <Link to="/sign-up"><Button size="huge" className="centered">{'Let\'s get started!'}</Button></Link>
 
   </div>
 );
