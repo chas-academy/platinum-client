558141e333f03d0cf30e8aa6141d8050a6bdebfe
jonastore <jonas.toremar@chasacademy.se>
2018-05-15 12:15:21 +0200

    delete student declarations

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,6 +1,5 @@
 import Admin from './Admin';
 import IsSignedIn from './IsSignedIn';
-import Students from './Students';
 import Polls from './Polls';
 import Questionnaires from './Questionnaires';
 import Users from './Users';
@@ -8,7 +7,6 @@ import Users from './Users';
 export default {
   AdminUsers: Admin.Users,
   IsSignedIn,
-  Students,
   Polls,
   Questionnaires,
   Users,
--- src/Views/Site/index.js
+++ src/Views/Site/index.js
@@ -1,7 +1,6 @@
 import Home from './Home';
 import MyProfile from './MyProfile';
 import PageNotFound from './PageNotFound';
-import Redux from './Redux';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Pricing from './Pricing';
@@ -17,7 +16,6 @@ export default {
   Home,
   MyProfile,
   PageNotFound,
-  Redux,
   SignIn,
   SignUp,
   Pricing,
