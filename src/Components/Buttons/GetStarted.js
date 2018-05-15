2d21ad6077275ba72b4e95494380ad3e9c9e618e
jonastore <jonas.toremar@chasacademy.se>
2018-05-15 15:55:59 +0200

    change button link if user or logged in or not

--- src/Components/Buttons/GetStarted.js
+++ src/Components/Buttons/GetStarted.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import * as Session from '../../Lib/Helpers/Session';
 
-// onClick={this.redirect('/start') basically linking to the start spage.
-// maybe there's a better way than using this one-line crap
 
 const GetStarted = () => (
   <div className="ui center aligned basic segment">
 
+    { !Session.isSignedIn() &&
     <Link to="/sign-up"><Button size="huge" className="centered">{'Let\'s get started!'}</Button></Link>
-
+  }
+    { Session.isSignedIn() &&
+    <Link to="/create-questionnaire"><Button size="huge" className="centered">{'Let\'s get started!'}</Button></Link>
+  }
   </div>
 );
 
