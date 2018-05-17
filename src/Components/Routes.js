e73aadc3b5eba94e5fadbc395b9dfae0e5584d25
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-17 14:51:36 +0200

    remove unused routes adn fix redirect

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -15,23 +15,18 @@ export default class Routes extends Component {
           <SiteRoute exact path="/sign-in" component={Site.SignIn} />
           <SiteRoute exact path="/sign-up" component={Site.SignUp} />
           <SiteRoute exact path="/pricing" component={Site.Pricing} />
+          <SiteRoute exact path="/about" component={Site.About} />
+          <SiteRoute exact path="/polls/:pollId" component={Site.Polls} />
+          <SiteRoute path="/polls/:pollId/result" component={Site.Result} />
           <AuthSiteRoute exact path="/create-questionnaire" component={Site.CreateQuestionnaire} />
           <AuthSiteRoute exact path="/my-questionnaires" component={Site.MyQuestionnaires} />
           <AuthSiteRoute exact path="/my-polls" component={Site.MyPolls} />
-          <SiteRoute exact path="/about" component={Site.About} />
           <AuthSiteRoute exact path="/howdoesitwork" component={Site.HowDoesItWork} />
-          <SiteRoute exact path="/polls/:pollId" component={Site.Polls} />
-          <SiteRoute path="/polls/:pollId/result" component={Site.Result} />
 
           <AdminRoute
             exact
             path="/admin"
-            component={() => <Redirect to="/admin/dashboard" />}
-          />
-          <AdminRoute
-            exact
-            path="/admin/dashboard"
-            component={Admin.Dashboard}
+            component={() => <Redirect to="/admin/users" />}
           />
           <AdminRoute exact path="/admin/users" component={Admin.Users.List} />
           <AdminRoute
@@ -49,7 +44,6 @@ export default class Routes extends Component {
             path="/admin/users/:userId/edit"
             component={Admin.Users.Edit}
           />
-          <AdminRoute exact path="/admin/settings" component={Admin.Settings} />
           <AdminRoute path="/admin/*" component={Admin.PageNotFound} />
 
           <SiteRoute path="*" component={Site.PageNotFound} />
