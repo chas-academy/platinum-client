21537b64e2d19464b961ee316fe0b9a32ef0a745
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-22 16:25:08 +0200

    connect new actions and props to SignIn component

--- src/Redux/Containers/Sessions/SignIn.js
+++ src/Redux/Containers/Sessions/SignIn.js
@@ -1,14 +1,21 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { auth } from '../../Actions/Sessions';
+import { loginUser } from '../../Actions/Users';
 import SignInForm from '../../../Components/Forms/SignIn';
 
-function mapStateToProps({ IsSignedIn }) {
-  return { IsSignedIn };
+function mapStateToProps(state) {
+  return {
+    isSignedIn: state.IsSignedIn,
+    isSigningIn: state.Users.isSigningIn,
+    redirect: state.Users.redirect,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ auth }, dispatch);
+  return {
+    auth: IsSignedIn => dispatch(auth(IsSignedIn)),
+    loginUser: data => dispatch(loginUser(data)),
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignInForm);
