83578ed1c22367a8f2a5f682905291dd0568bd8b
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-05-23 14:59:58 +0200

    Update color scheme

--- src/Components/Results/BarChart.js
+++ src/Components/Results/BarChart.js
@@ -6,7 +6,7 @@ export default class BarChartResult extends Component {
   render() {
     const data = [];
     const labels = [];
-    const colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
+    const colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe', '#008080', '#e6beff', '#aa6e28', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000080', '#808080', '#000000'];
 
     this.props.options.forEach((element, index) => {
       const option = {
