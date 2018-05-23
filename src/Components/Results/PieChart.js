25e5e8dc598175ad4d05ce08ff23e7ab670671aa
Izabel Rosén <izabel.rosen@chasacademy.se>
2018-05-23 14:59:21 +0200

    Custome legend result

--- src/Components/Results/PieChart.js
+++ src/Components/Results/PieChart.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import { PieChart, Pie, Cell, Legend, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import uuidv4 from 'uuid/v4';
 /* eslint-disable react/prop-types, no-mixed-operators */
 export default class PieChartResult extends Component {
   render() {
     const data = [];
+    const labels = [];
+    const colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe', '#008080', '#e6beff', '#aa6e28', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000080', '#808080', '#000000'];
 
-    this.props.options.forEach((element) => {
+    this.props.options.forEach((element, index) => {
       const option = {
         name: element.name,
         value: element.votes,
       };
 
+      const color = {
+        fill: colors[index],
+      };
+      const label = (
+        <div className="bar__legend" key={uuidv4()}>
+          <svg className="bar__legend-svg" width="10" height="10">
+            <rect width="10" height="10" style={color} />
+          </svg>
+          <p>{element.name}</p>
+        </div>
+      );
+
+      labels.push(label);
       data.push(option);
     });
-    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
     const RADIAN = Math.PI / 180;
     const renderCustomizedLabel = ({
@@ -38,24 +52,28 @@ export default class PieChartResult extends Component {
     };
 
     return (
-      <ResponsiveContainer height={400}>
-        <PieChart width={800} height={400} onMouseEnter={this.onPieEnter} >
-          <Pie
-            verticalAlign="middle"
-            data={data}
-            dataKey="value"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-          >
-            {
-          data.map((entry, index) => <Cell key={uuidv4()} fill={COLORS[index % COLORS.length]} />)
+      <div>
+        <ResponsiveContainer height={400}>
+          <PieChart width={800} height={400} onMouseEnter={this.onPieEnter} >
+            <Pie
+              verticalAlign="middle"
+              data={data}
+              dataKey="value"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+            >
+              {
+          data.map((entry, index) => <Cell key={uuidv4()} fill={colors[index % colors.length]} />)
             }
-          </Pie>
-          <Legend />
-        </PieChart>
-      </ResponsiveContainer>
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+        <div className="bar__legend-labels">
+          {labels}
+        </div>
+      </div>
     );
   }
 }
