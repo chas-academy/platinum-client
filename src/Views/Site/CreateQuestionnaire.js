1fe4b7f5f817881ef0ca768c0816616a4ef278fc
´SebastianGerSe <sebastian.gerstel.sollerman@chasacademy.se>
2018-05-24 11:55:51 +0200

    add page title and lable in create questionnaire

--- src/Views/Site/CreateQuestionnaire.js
+++ src/Views/Site/CreateQuestionnaire.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { PageTitle } from '../../Lib/Common/Views';
 import Questionnaire from '../../Redux/Containers/Questionnaires/CreateQuestionnaire';
 /* eslint-disable react/prop-types */
-const CreateQuestionnaire = props => <Questionnaire location={props.location} />;
+const CreateQuestionnaire = props => (
+  <div>
+    <PageTitle title="Create Questionnaire" />
+    <Questionnaire location={props.location} />
+  </div>);
 
 export default CreateQuestionnaire;
